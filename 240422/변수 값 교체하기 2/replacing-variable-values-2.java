6a0564626fa48ce5d8d9747083f21a4b2aeb14fd
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-22 16:59:06 +0900

    Title: 변수 값 교체하기 2, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240422/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        a = b;
+        // 출력
+        System.out.print(a);
+        System.out.print("\n");
+        System.out.print(b);
+    }
+}
