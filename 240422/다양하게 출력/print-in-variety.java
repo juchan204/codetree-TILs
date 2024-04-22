72ac6d65bc424b8395121cdc088e3f1e0b56ca0a
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-22 15:15:36 +0900

    Title: 다양하게 출력, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240422/다양하게 출력/print-in-variety.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Total days in Year");
+        System.out.print("\n");
+        System.out.print(365);
+        System.out.print("\n");
+        System.out.print("Circumference rate");
+        System.out.print("\n");
+        System.out.print(3.1415926535);
+    }
+}
