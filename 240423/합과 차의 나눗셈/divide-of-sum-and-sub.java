d7d8861c480fa0513568d7720e1c7211b92cb27e
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-23 14:19:48 +0900

    Title: 합과 차의 나눗셈, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240423/합과 차의 나눗셈/divide-of-sum-and-sub.java
+++ 240423/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.useDelimiter(" ");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        double result = (double) (a + b) / (a - b); 
-        System.out.printf("%.2f", result);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        // (a-b)가 0이 되는 경우를 방지
+        if (a == b) {
+            System.out.println("Error: Division by zero");
+        } else {
+            double result = (double) (a + b) / (a - b);
+            System.out.printf("%.2f", result);
+        }
+
+        scanner.close();
     }
 }
