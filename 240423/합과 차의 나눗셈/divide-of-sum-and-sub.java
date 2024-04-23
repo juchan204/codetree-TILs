b65771ea9b481670d12a4d4b38df84d5a5fbda77
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-23 14:05:43 +0900

    Title: 합과 차의 나눗셈, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240423/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(" ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        float c = (a+b);
+        float d = (a-b);
+        float f = c/d;
+
+        System.out.printf("%.2f",f);
+
+    }
+}
