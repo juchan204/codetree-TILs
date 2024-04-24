8d58b88c5d12d327f8dae12e752fc278f48d6abb
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-24 09:04:13 +0900

    Title: 최대 2번의 연산, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240424/최대 2번의 연산/up-to-2-calculations.java
+++ 240424/최대 2번의 연산/up-to-2-calculations.java
@@ -5,16 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
+        // a가 짝수일 때
         if (a % 2 == 0) {
-            int b = a/2;
-
-            if (b % 2 == 1) {
-                System.out.println( (b + 1)/2 );
-            }
+            int b = a / 2;
+            // b가 홀수이거나 짝수인 경우 모두 포함하여 처리
+            System.out.println((b + 1) / 2);
         }
-
-        if (a % 2 == 1) {
-            System.out.println( (a + 1)/2 );
+        // a가 홀수일 때
+        else {
+            System.out.println((a + 1) / 2);
         }
 
         
