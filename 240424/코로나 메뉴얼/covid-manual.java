1ca77da734f4c943677c2293706d865fa1411b7d
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-24 23:21:46 +0900

    Title: 코로나 메뉴얼, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240424/코로나 메뉴얼/covid-manual.java
+++ 240424/코로나 메뉴얼/covid-manual.java
@@ -4,39 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 변수 선언
-        int a, b, c;
-        char a_cold , b_cold , c_cold;
-        
-        // 입력
-        a_cold = sc.next().charAt(0);
-        a = sc.nextInt();
-        b_cold = sc.next().charAt(0);
-        b = sc.nextInt();
-        c_cold = sc.next().charAt(0);
-        c = sc.nextInt();
-        
+        // 변수 선언 및 입력
+        char a_cold = sc.next().charAt(0);
+        int a = sc.nextInt();
+        char b_cold = sc.next().charAt(0);
+        int b = sc.nextInt();
+        char c_cold = sc.next().charAt(0);
+        int c = sc.nextInt();
+
+        // 열이 있는 사람 수와 37도 이상인 사람 수 세기
+        int feverCount = 0;
+        if (a_cold == 'Y' && a >= 37) feverCount++;
+        if (b_cold == 'Y' && b >= 37) feverCount++;
+        if (c_cold == 'Y' && c >= 37) feverCount++;
 
         // 출력
-        if(a_cold == 'Y' && a >= 37) {
-            if(b_cold == 'Y' && b >= 37 || c_cold == 'Y' && c >= 37)
-                System.out.println("E");
-            else
-                System.out.println("N");
-        }
-        else if(b_cold == 'Y' && b >= 37) {
-            if(a_cold == 'Y' && a >= 37 || c_cold == 'Y' && c >= 37)
-                System.out.println("E");
-            else
-                System.out.println("N");
-        }
-        else if(c_cold == 'Y' && c >= 37) {
-            if(b_cold == 'Y' && b >= 37 || a_cold == 'Y' && a >= 37)
-                System.out.println("E");
-            else
-                System.out.println("N");
+        if (feverCount >= 2) {
+            System.out.println("E");
+        } else if (feverCount == 1) {
+            System.out.println("N");
+        } else {
+            // 아무도 열이 없거나 조건을 만족하지 않는 경우
+            // 이 부분은 문제의 요구사항에 따라 다를 수 있으며, 필요에 따라 추가 또는 생략 가능
         }
 
-        
     }
 }
