c975d5b19943542db7f9bea83a7753de5dc1222d
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-24 23:04:17 +0900

    Title: 최댓값 구하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240424/최댓값 구하기/maximum-value.java
+++ 240424/최댓값 구하기/maximum-value.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
 
-        if (a > b && a > c || a == b && a  > c || a == c && a  > b ) {
-            System.out.println(a);
+        // 변수 선언
+        int a, b, c;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        // 출력
+        // a와 b를 비교한뒤, a가 b보다 크다면 a와 c를 비교하여 최댓값을 구합니다.
+        if(a >= b) {
+            if(a >= c)
+                System.out.println(a);
+            else
+                System.out.println(c);
         }
 
-        else if ( b > a && b > c || a == b && b  > c || b == c && b  > a){
-            System.out.println(b);
-        }
-
+        // a와 b를 비교한 결과가 나와있으므로, b와 c만 비교하여 최댓값을 구합니다.
         else {
-            System.out.println(c);
+            if(b >= c)
+                System.out.println(b);
+            else
+                System.out.println(c);
         }
     }
-    
 }
