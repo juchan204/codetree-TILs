857271d0b42856789ad075369273b4486cc0b010
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-24 19:35:42 +0900

    Title: 두 사람, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240424/두 사람/two-person.java
+++ 240424/두 사람/two-person.java
@@ -6,22 +6,18 @@ public class Main {
 
         // 변수 선언
         int a_age, b_age;
-        String a_sex, b_sex;
+        char a_gen, b_gen;
         
         // 입력
-        a_age = sc.nextInt(); // 첫 번째 사람의 나이
-        a_sex = sc.next(); // 첫 번째 사람의 성별
-        b_age = sc.nextInt(); // 두 번째 사람의 나이
-        b_sex = sc.next(); // 두 번째 사람의 성별
+        a_age = sc.nextInt();
+        a_gen = sc.next().charAt(0);
+        b_age = sc.nextInt();
+        b_gen = sc.next().charAt(0);
 
         // 출력
-        // 첫 번째 사람이나 두 번째 사람 중 한 사람이라도 19세 이상이면서 남자라면 1을 출력
-        if ((a_age >= 19 && a_sex.equals("M")) || (b_age >= 19 && b_sex.equals("M"))) {
+        if((a_age >= 19 && a_gen == 'M') || (b_age >= 19 && b_gen == 'M'))
             System.out.println("1");
-        } else {
+        else
             System.out.println("0");
-        }
-
-        sc.close(); // Scanner 객체 닫기
     }
 }
