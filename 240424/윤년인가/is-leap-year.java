c1e53f465509e13b54eb0f942451bcf168a1ca64
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-24 22:43:13 +0900

    Title: 윤년인가, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240424/윤년인가/is-leap-year.java
+++ 240424/윤년인가/is-leap-year.java
@@ -5,14 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
-        if (a % 4 == 0) {
-            if (a % 100 == 0 && a % 400 != 0 ) {
-                System.out.println("false");
+        if (a % 4 != 0 || a % 100 == 0 && a % 400 != 0 ) {
+            System.out.println("false");
             }
-            else {
-                System.out.println("true");
+        else {
+            System.out.println("true");
             }
-        }
-        
     }
+    
 }
