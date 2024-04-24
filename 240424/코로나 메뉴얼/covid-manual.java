0bc03e382876c822f3d83567327bb8f1e615640e
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-24 23:22:44 +0900

    Title: 코로나 메뉴얼, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240424/코로나 메뉴얼/covid-manual.java
+++ 240424/코로나 메뉴얼/covid-manual.java
@@ -4,26 +4,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 변수 선언 및 입력
-        char a_cold = sc.next().charAt(0);
-        int a = sc.nextInt();
-        char b_cold = sc.next().charAt(0);
-        int b = sc.nextInt();
-        char c_cold = sc.next().charAt(0);
-        int c = sc.nextInt();
-
-        // 열이 있는 사람 수와 37도 이상인 사람 수 세기
-        int feverCount = 0;
-        if (a_cold == 'Y' && a >= 37) feverCount++;
-        if (b_cold == 'Y' && b >= 37) feverCount++;
-        if (c_cold == 'Y' && c >= 37) feverCount++;
+        // 변수 선언
+        char c1, c2, c3;
+        int t1, t2, t3;
+        
+        // 입력
+        c1 = sc.next().charAt(0);
+        t1 = sc.nextInt();
+        c2 = sc.next().charAt(0);
+        t2 = sc.nextInt();
+        c3 = sc.next().charAt(0);
+        t3 = sc.nextInt();
 
         // 출력
-        if (feverCount >= 2) {
-            System.out.println("E");
-        } else if (feverCount == 1) {
-            System.out.println("N");
-        } 
-
+        // A가 2명 이상인지 판단하기
+        if(c1 == 'Y' && t1 >= 37) {
+            // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+            if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
+                System.out.println("E");
+            else
+                System.out.println("N");
+        }
+        else {
+            // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+            if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
+                System.out.println("E");
+            else
+                System.out.println("N");
+        }
     }
 }
