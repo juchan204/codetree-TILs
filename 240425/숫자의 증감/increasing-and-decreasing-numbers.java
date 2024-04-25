98b50312fc6ad2eed76dd3d48a3ecdfaaa8f3cda
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-25 15:44:49 +0900

    Title: 숫자의 증감, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240425/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240425/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -9,14 +9,15 @@ public class Main {
         
 
         if (c == 'A') {
+            // 'A'를 입력받았을 때, 1부터 n까지 숫자를 증가시키며 출력합니다.
             for (int i = 1; i <= n; i++) {
                 System.out.print(i + " ");
             }
-        }
-
-        else if ( c == 'D'){
+        } else if (c == 'D') {
+            // 'D'를 입력받았을 때, n부터 1까지 숫자를 감소시키며 출력합니다.
+            // 여기서 i는 n에서 시작해서 1까지 감소해야 하므로 초기 조건, 조건식, 그리고 증감식을 수정해야 합니다.
             for (int i = n; i >= 1; i--) {
-                System.out.println(i + " ");
+                System.out.print(i + " ");
             }
         }
 
