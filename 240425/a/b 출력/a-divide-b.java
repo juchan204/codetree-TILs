3663512e1aacd825abb884de892ea5fac48c5b77
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-25 15:29:32 +0900

    Title: a/b 출력, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240425/a/b 출력/a-divide-b.java
+++ 240425/a/b 출력/a-divide-b.java
@@ -3,10 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
         
-        System.out.printf("%.20f",(double) a/b);
+        // 변수 선언
+        int a, b;
+
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        // 정수 부분을 먼저 출력합니다.
+        System.out.print(a / b + ".");
+        
+        // a를 b로 나눈 나머지를 시작으로
+        // 소수점 자리를 하나씩 계산합니다.
+        a %= b;
+        for(int i = 0; i < 20; i++) {
+            // 나머지에 10 곱한 값을 기준으로
+            // b로 나누었을 떄의 몫을 구해줍니다.
+            a *= 10;
+            System.out.print(a / b);
+            
+            // a를 b로 나눈 나머지를 게속 갱신해줍니다.
+            a %= b;
+        }
     }
 }
