f35be0d967d17ae4beb1db80f13f5dea9fbb6be2
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-25 14:59:48 +0900

    Title: 별 n개 출력하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240425/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  
+        int i = 1 ;            
+
+        while (i <= n) {           // 계속 반복할 조건(조건식)
+            System.out.println("*");  // 원하는 코드 작성
+            i++;                    // 원하는 증감값 설정(증감식)
+        }
+
+        
+    }
+}
