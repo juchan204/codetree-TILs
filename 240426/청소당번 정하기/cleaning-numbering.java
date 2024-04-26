9a5170a578a1d345c9b9724162a23f5fc7ddd609
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-26 20:03:23 +0900

    Title: 청소당번 정하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240426/청소당번 정하기/cleaning-numbering.java
+++ 240426/청소당번 정하기/cleaning-numbering.java
@@ -4,27 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int cnt2 = 0, cnt3 = 0, cnt12 = 0 ;
+        int n = sc.nextInt(); // n일 동안
+        int cntClassroom = 0, cntCorridor = 0, cntToilet = 0;
 
-        for( int i = 1 ; i <= a ; i++){
-            if ( i % 2 == 0 )
-                cnt2 ++;
-            if ( i % 3 == 0 )
-                cnt3 ++;
-            if ( i % 12 == 0 )
-                cnt12 ++;
-            if ( i % 2 == 0 && i % 3 == 0)
-                cnt2 --;
-            if ( i % 2 == 0 && i % 12 == 0)
-                cnt2 --;    
-            if ( i % 3 == 0 && i % 12 == 0)
-                cnt3 --;
-            if ( i % 2 == 0 && i % 3 == 0 && i % 12 == 0) {
-                cnt2 --;
-                cnt3 --;
-            }          
+        for (int i = 1; i <= n; i++) {
+            // 12일마다 화장실 청소를 우선합니다.
+            if (i % 12 == 0) {
+                cntToilet++;
+            }
+            // 그 다음으로 3일마다 복도 청소를 고려합니다. 단, 화장실 청소일에는 제외합니다.
+            else if (i % 3 == 0) {
+                cntCorridor++;
+            }
+            // 마지막으로 2일마다 교실 청소를 고려합니다. 단, 화장실 또는 복도 청소일에는 제외합니다.
+            else if (i % 2 == 0) {
+                cntClassroom++;
+            }
         }
-        System.out.print(cnt2 + " " + cnt3 + " " + cnt12);
+        // 각 장소별 청소 횟수 출력
+        System.out.println(cntClassroom + " " + cntCorridor + " " + cntToilet);
     }
 }
