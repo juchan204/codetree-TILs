8bddd78c5749ace6b058740519f855952ee600a8
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-26 20:03:40 +0900

    Title: 청소당번 정하기, Time: 255ms, Memory: 10MB, Status: Passed - CodeTree

--- 240426/청소당번 정하기/cleaning-numbering.java
+++ 240426/청소당번 정하기/cleaning-numbering.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
 
-        int n = sc.nextInt(); // n일 동안
-        int cntClassroom = 0, cntCorridor = 0, cntToilet = 0;
-
-        for (int i = 1; i <= n; i++) {
-            // 12일마다 화장실 청소를 우선합니다.
-            if (i % 12 == 0) {
-                cntToilet++;
-            }
-            // 그 다음으로 3일마다 복도 청소를 고려합니다. 단, 화장실 청소일에는 제외합니다.
-            else if (i % 3 == 0) {
-                cntCorridor++;
-            }
-            // 마지막으로 2일마다 교실 청소를 고려합니다. 단, 화장실 또는 복도 청소일에는 제외합니다.
-            else if (i % 2 == 0) {
-                cntClassroom++;
-            }
+        int cnt2 = 0, cnt3 = 0, cnt12 = 0;
+    
+        // 각 날짜마다 확인합니다.
+        for(int i = 1; i <= n; i++) {
+            // 주기가 가장 긴 12일부터 확인합니다.
+            if(i % 12 == 0)
+                cnt12++;
+            // 12일 주기에 들어오지 않는다면, 3일 주기에 들어오는지 확인합니다.
+            else if(i % 3 == 0)
+                cnt3++;
+            // 3일 주기에도 들어오지 않는다면, 2일 주기에 들어오는지 확인합니다.
+            else if(i % 2 == 0)
+                cnt2++;
         }
-        // 각 장소별 청소 횟수 출력
-        System.out.println(cntClassroom + " " + cntCorridor + " " + cntToilet);
+
+        System.out.println(cnt2 + " " + cnt3 + " " + cnt12);
     }
 }
