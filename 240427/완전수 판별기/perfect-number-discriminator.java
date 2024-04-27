635706d468a3ea5ed8e446ab304252b4a8738110
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-27 16:37:53 +0900

    Title: 완전수 판별기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240427/완전수 판별기/perfect-number-discriminator.java
+++ 240427/완전수 판별기/perfect-number-discriminator.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        
+        // 변수 선언 및 입력
+        int n;
+        int sum = 0;
 
-        int sumVal = 0 ;
+        n = sc.nextInt();
 
-        for ( int i = 1 ; i <= a ; i++){
-            if ( a % i == 0 && a != i){
-                sumVal += i;
-            }
+        // 1부터 n-1까지의 수 중에서 약수를 찾습니다.
+        for(int i = 1; i < n; i++) {
+            if(n % i == 0)
+                sum += i;
         }
 
-        if (sumVal == a){
-            System.out.println("P");
-        }
-        else {
-            System.out.println("N");
-        }
+        // sum과 n이 같다면 P을, 다르다면 N을 출력합니다.
+        if(sum == n)
+            System.out.print("P");
+        else
+            System.out.print("N");
     }
 }
