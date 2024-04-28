81607dc361fca7d1aa449d41fb05e7b67c27b34b
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-28 21:37:42 +0900

    Title: 1부터 곱셈하기, Time: 250ms, Memory: 10MB, Status: Passed - CodeTree

--- 240428/1부터 곱셈하기/multiple-from-one.java
+++ 240428/1부터 곱셈하기/multiple-from-one.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int i ;
-        int sum = 1;
+        
+        // 변수 선언
+        int n;
+        int prod = 1;
 
-        for ( i = 1 ; i <= 10 ; i++){
-            sum = sum * i;
+        n = sc.nextInt();
 
-            if ( sum >= n )
+        // 1부터 증가시키며 곱한 값이 n이상이 된 순간, 곱해진 숫자를 출력합니다.
+        for(int i = 1; i <= 10; i++) {
+            prod *= i;
+            if(prod >= n) {
+                System.out.print(i);
                 break;
-       }
-
-       System.out.print(i);
-    }   
-
+            }
+        }
+    }
 }
