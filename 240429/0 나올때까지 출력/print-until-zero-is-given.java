ea30df486a0d25adc7b4b6a09ce746f2bd94839c
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-29 13:27:21 +0900

    Title: 0 나올때까지 출력, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240429/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        
+        while (true) {
+            n = sc.nextInt();
+            
+            if (n != 0) {
+                System.out.println(n);
+                
+            }
+            else{
+                break;
+            }
+        }
+        
+    }
+}
