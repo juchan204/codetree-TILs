74dc53eb448fd02f5c4d47ff9e2c7a062dfa8a12
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-04-29 13:33:19 +0900

    Title: 특정 숫자 맞추기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240429/특정 숫자 맞추기/catching-specific-number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        
+
+        while (true) {
+            n = sc.nextInt();
+            
+            if (n > 25) {
+                System.out.println("Lower");
+                
+            }
+            else if (n < 25){
+                System.out.println("Higher");
+            }
+            else {
+                System.out.println("Good");
+                break;
+            }
+                
+
+            
+        }
+        
+    }
+}
