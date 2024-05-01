a339f5fce1083d2e3496c165af3fcbdf7f61c82a
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-01 19:02:16 +0900

    Title: 합성수 판별기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240501/합성수 판별기/factorization-discriminator.java
+++ 240501/합성수 판별기/factorization-discriminator.java
@@ -6,7 +6,7 @@ public class Main {
         int a = sc.nextInt();
 
         boolean satisfied = false;
-        for (int i = 1; i <= a; i++) {
+        for (int i = 2; i < a; i++) {
             if (a % i == 0) {
                 satisfied = true;
             }
