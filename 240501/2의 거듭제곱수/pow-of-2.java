96d5345cb6c8a051e3975c81640d49617c91103b
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-01 18:25:12 +0900

    Title: 2의 거듭제곱수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240501/2의 거듭제곱수/pow-of-2.java
+++ 240501/2의 거듭제곱수/pow-of-2.java
@@ -23,3 +23,28 @@ public class Main {
         System.out.println(cnt);
     }
 }
+
+import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         // 변수 선언 및 입력
+//         int n;
+//         n = sc.nextInt();
+//         int prod = 1;
+// 	    int x = 0;
+
+//         while(true) {
+//             // prod가 n이 될 때까지 2를 곱합니다.
+//             if(n == prod)
+//                 break;
+            
+//             prod *= 2;
+//             x++;
+//         }
+
+//         System.out.println(x);
+//     }
+// }
