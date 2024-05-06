34f142aeb299704b3f603bb631b952bfd6edf6ec
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-07 07:57:23 +0900

    Title: 별표 출력하기 13, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240507/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 사용자로부터 n의 값을 입력받음
+
+        for (int i = 0; i <= 2 * n - 1; i++) { // 총 2n-1줄로 구성된 패턴 생성
+            if (i % 2 == 0) {
+                
+                for (int j = 0; j < n - i / 2; j++) {
+                    System.out.print("* ");
+                }
+            }
+            else {
+                
+                for (int j = 0; j < 1 + (i / 2); j++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println(); // 현재 줄의 별 출력을 마친 후 다음 줄로 넘어감
+        }
+    }
+}
