914427157e9ff2b37ec3b0acf78837721a15dfa5
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-07 08:47:40 +0900

    Title: 규칙을 찾아 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240507/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240507/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -23,3 +23,22 @@ public class Main {
         }
     }
 }
+import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // 변수 선언 및 입력
+//         int n = sc.nextInt();
+
+//         // n칸의 정사각형에서 i, j가 조건에 맞으면 *을 출력합니다.
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(i > j || i == 0 || j == n - 1) System.out.print("* ");
+//                 else System.out.print("  ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
