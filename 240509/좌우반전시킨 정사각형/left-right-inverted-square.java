916b6c893a19caa5ca9bade1385572cde97eb2c8
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-09 16:38:56 +0900

    Title: 좌우반전시킨 정사각형, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240509/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        // n칸의 정사각형에 올바른 숫자를 출력합니다.
+        for(int i = 1; i <= n; i++) {
+            for(int j = 4; j > 0 ; j--) {
+                System.out.print( i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
