3f2a4fd29d5724d24592bae41dfaa97488a01539
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-09 16:47:33 +0900

    Title: 1-9 정사각형, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240509/1-9 정사각형/1-9-square.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = 1;
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(cnt);
+                cnt++;
+                if ( cnt == 10){
+                    cnt = 1 ;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
