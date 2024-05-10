bc0611db20c47fd496b37f461212811341cb37ed
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-10 15:56:35 +0900

    Title: 좌우 지그재그 출력, Time: 232ms, Memory: 12MB, Status: Passed - CodeTree

--- 240510/좌우 지그재그 출력/print-lr-zigzag.java
+++ 240510/좌우 지그재그 출력/print-lr-zigzag.java
@@ -7,7 +7,6 @@ public class Main {
         // 변수 선언 및 입력
         int n = sc.nextInt();
 
-        // 상하로 반복하여 출력합니다.
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if(i % 2 == 0)
