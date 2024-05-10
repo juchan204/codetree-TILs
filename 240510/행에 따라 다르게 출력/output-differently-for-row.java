ceb9b7f88a5ea380ddc6f149d2126b0a1b2b43de
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-10 17:11:29 +0900

    Title: 행에 따라 다르게 출력, Time: 218ms, Memory: 12MB, Status: Passed - CodeTree

--- 240510/행에 따라 다르게 출력/output-differently-for-row.java
+++ 240510/행에 따라 다르게 출력/output-differently-for-row.java
@@ -4,28 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 사용자로부터 n 값을 입력받음
-        int n = sc.nextInt();
+        int n = sc.nextInt();  // 사용자로부터 숫자 입력 받기
 
-        int start = 1; // 시작 숫자
+        int currentNum = 0; // 현재 숫자를 추적하기 위한 변수
 
-        for (int i = 1; i <= n; i++) { // n 줄을 위한 반복문
-            int current = start; // 현재 줄에서의 숫자 시작점
-            for (int j = 1; j <= n; j++) { // 각 줄에 n개의 숫자를 출력하기 위한 반복문
-                System.out.print(current + " ");
-                if (i % 2 == 1) { // 홀수 번째 줄인 경우
-                    current += 1; // 숫자가 1씩 증가
-                } else { // 짝수 번째 줄인 경우
-                    current += 2; // 숫자가 2씩 증가
+        for (int i = 0; i < n; i++) { // 각 행에 대해 반복
+            if (i % 2 == 0) { // 짝수 행인 경우 (0부터 시작하므로 실제로는 홀수 번째 행)
+                for (int j = 0; j < n; j++) { // 각 열에 대해 반복
+                    currentNum++; // 숫자 증가
+                    System.out.print(currentNum + " "); // 현재 숫자 출력
+                    
+                }
+            } 
+            else { // 홀수 행인 경우 (실제로는 짝수 번째 행)
+                for (int j = 0; j < n; j++) {
+                    currentNum += 2;
+                    System.out.print(currentNum + " ");
                 }
             }
-            System.out.println();
-            if (i % 2 == 1) { // 다음 줄의 시작 숫자를 결정하기 위한 조건문
-                start += n + 1; // 홀수 줄 다음에는 n만큼 증가
-            } else {
-                start += n + 2; // 짝수 줄 다음에는 n+2만큼 증가
-            }
-        
+            System.out.println(); // 행이 끝나면 줄바꿈
         }
     }
 }
