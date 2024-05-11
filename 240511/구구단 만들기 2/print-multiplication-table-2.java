b7ab69a25381992b55bcb756a33b3ca1857f20f8
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-11 17:23:15 +0900

    Title: 구구단 만들기 2, Time: 190ms, Memory: 10MB, Status: Passed - CodeTree

--- 240511/구구단 만들기 2/print-multiplication-table-2.java
+++ 240511/구구단 만들기 2/print-multiplication-table-2.java
@@ -21,3 +21,25 @@ public class Main {
         }
     }
 }
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // 변수 선언 및 입력
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         // 조건대로 구구단을 출력합니다.
+//         for(int i = 2; i <= 8; i += 2) {
+//             for(int j = b; j >= a; j--) {
+//                 System.out.print(j + " * " + i + " = " + (i * j));
+//                 if(j != a)
+//                     System.out.print(" / ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
