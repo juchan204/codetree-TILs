441fd82a9ddbc27e97faff741c87e59c1324ab9b
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-13 14:44:16 +0900

    Title: 19단 출력, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240513/19단 출력/nineteen-times-table.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 19; i++) {
+            for (int j = 1; j <= 19; j++) {
+                if ( j % 2 == 0 )
+                    System.out.println(i + " * " + j + " = " + (i * j));
+                else if ( j == 19 )
+                    System.out.println(i + " * " + j + " = " + (i * j));
+                else
+                    System.out.print(i + " * " + j + " = " + (i * j) + " / ");
+            }
+
+        }
+    }
+}
