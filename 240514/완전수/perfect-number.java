26349bf1894f4d892b1ece985f86515a861fb77b
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-14 21:40:57 +0900

    Title: 완전수, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240514/완전수/perfect-number.java
+++ 240514/완전수/perfect-number.java
@@ -25,3 +25,32 @@ public class Main {
         System.out.println(perfectNumbersCount); // 완전수의 개수를 출력
     }
 }
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // 변수 선언 및 입력
+//         int start = sc.nextInt();
+//         int end = sc.nextInt();
+//         int answer_cnt = 0;
+
+//         for(int curr_num = start; curr_num <= end; curr_num++) {
+//             // Step 1:
+//             int divisor_sum = 0;
+//             for(int divisor = 1; divisor <= curr_num-1; divisor++){
+//                 if(curr_num % divisor == 0){
+//                     divisor_sum += divisor;
+//                 }
+//             }
+//             // Case 1:
+//             if(divisor_sum == curr_num){
+//                 answer_cnt++;
+//             }
+//         }
+        
+//         System.out.println(answer_cnt);
+//     }
+// }
