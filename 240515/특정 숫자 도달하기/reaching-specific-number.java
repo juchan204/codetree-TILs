18ffd712f970511a626a04bf60899020787d3f52
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-15 14:47:28 +0900

    Title: 특정 숫자 도달하기, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

--- 240515/특정 숫자 도달하기/reaching-specific-number.java
+++ 240515/특정 숫자 도달하기/reaching-specific-number.java
@@ -4,17 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 변수 선언
         int[] arr = new int[10];
+
         int sum = 0;
         int cnt = 0;
-        
-        // 10개의 정수를 입력받습니다.
-        for(int i = 0; i < 10; i++) {
+
+        for(int i = 0; i < 10; i++){
             arr[i] = sc.nextInt();
         }
 
-        // 10개의 정수 중 250 이상의 수가 나올 때 까지의 수의 합계와 개수를 구합니다.
         for (int i = 0; i < 10; i++) {
             if (arr[i] >= 250) {
                 break;
@@ -22,7 +20,6 @@ public class Main {
             sum += arr[i];
             cnt++ ;
         }
-        
         //실수 나눗셈 표현
         System.out.printf("%d %.1f",sum,(double)sum/cnt);
     }
