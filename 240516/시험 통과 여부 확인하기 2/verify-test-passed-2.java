c6c91326f4f47a3535f5c25e4fa0b436f4b0309c
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-16 14:35:56 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240516/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 정수 배열 선언
+        int[] arr = new int[10];
+
+        int cnt = 0;
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = 0; j < 4; j++){
+                arr[j] = sc.nextInt();
+                sum += arr[j];
+            }   
+            double avg = (double) sum / 4;
+            if (avg >= 60){
+                System.out.println("pass");
+                cnt++;
+            }
+            else 
+                System.out.println("fail");
+        }
+        //출력
+        System.out.printf("%d", cnt);
+    }
+}
