f257ad386d0b6784a97b6e5fb35f7ef1518dd41a
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-16 09:42:16 +0900

    Title: 배열 채우기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240516/배열 채우기/filling-array.java
+++ 240516/배열 채우기/filling-array.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 변수 선언
         int[] arr = new int[10];
+        int i; // i를 for문 밖에서도 사용하기 위해 선언 위치를 변경합니다.
 
-        for(int i = 0; i < 10; i++) {
+        for(i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
             if (arr[i] == 0){
                 for(int j = i - 1 ; j >= 0; j--) {
@@ -16,5 +16,12 @@ public class Main {
                 break;
             }
         }
+        // 0이 입력되지 않고 모든 숫자가 입력된 경우
+        if (i == 10){ // i 변수의 범위를 정확히 체크합니다.
+            for(i = 9 ; i >= 0; i--) {
+                System.out.print(arr[i]+" ");
+            }
+        }
+            
     }
 }
