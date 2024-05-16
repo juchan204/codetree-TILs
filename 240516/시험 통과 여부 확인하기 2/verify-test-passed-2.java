13a10b6c8e25f3b31f1df1630cd3c235be84bc06
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-16 14:37:30 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240516/시험 통과 여부 확인하기 2/verify-test-passed-2.java
+++ 240516/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -4,13 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 정수 배열 선언
-        int[] arr = new int[10];
-
         int cnt = 0;
         int n = sc.nextInt();
 
         for(int i = 0; i < n; i++){
+            int[] arr = new int[10];
             int sum = 0;
             for(int j = 0; j < 4; j++){
                 arr[j] = sc.nextInt();
