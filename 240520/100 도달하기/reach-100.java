53215291195ba6f712ee1242448012875f968f5c
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-20 13:08:41 +0900

    Title: 100 도달하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240520/100 도달하기/reach-100.java
+++ 240520/100 도달하기/reach-100.java
@@ -10,7 +10,6 @@ public class Main {
         arr[1] = sc.nextInt();
         int cnt = 0;
 
-        // 입력받은 정수로부터 출력할 값을 새로운 배열에 저장하고, 0이 나오면 for문을 탈출
         for(int i = 2; i < 100; i++) {
             arr[i] = arr[i - 2] + arr[i - 1];
             cnt++;
@@ -21,7 +20,6 @@ public class Main {
             
         }
 
-        // 0이 나오기 전까지의 배열의 인덱스에 저장된 값을 출력
         for(int i = 0; i <= cnt; i++) {
             System.out.print(arr[i] + " ");
         }
