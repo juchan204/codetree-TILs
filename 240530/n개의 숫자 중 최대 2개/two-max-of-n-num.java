2218d70c75c237387e6ecc541dacb2c9db1fdff2
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-05-30 22:07:20 +0900

    Title: n개의 숫자 중 최대 2개, Time: 247ms, Memory: 10MB, Status: Passed - CodeTree

--- 240530/n개의 숫자 중 최대 2개/two-max-of-n-num.java
+++ 240530/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,29 +12,10 @@ public class Main {
             numbers[i] = sc.nextInt(); // 숫자 입력 받기
         }
 
-        // 첫 번째로 큰 수와 두 번째로 큰 수를 찾기 위한 변수 초기화
-        int maxNumber = numbers[0];
-        int secondMaxNumber = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            if (numbers[i] >= maxNumber) {
-                // 새로운 최댓값을 찾았을 때, 현재의 최댓값을 두 번째로 큰 수로 업데이트하고
-                // 새로운 최댓값으로 업데이트
-                secondMaxNumber = maxNumber;
-                maxNumber = numbers[i];
-            } else if (numbers[i] > secondMaxNumber && numbers[i] <= maxNumber) {
-                // 새로운 두 번째로 큰 수를 찾았을 때 업데이트
-                // 이 조건은 첫 번째로 큰 수와 동일한 값은 제외
-                secondMaxNumber = numbers[i];
-            }
-        }
-
-        // 첫 번째로 큰 수가 배열에 하나만 있을 때
-        if (secondMaxNumber == Integer.MIN_VALUE) {
-            secondMaxNumber = maxNumber; // 두 번째로 큰 수도 첫 번째로 큰 수와 동일
-        }
+        // 배열을 내림차순으로 정렬
+        Arrays.sort(numbers);
 
         // 가장 큰 수와 두 번째로 큰 수 출력
-        System.out.println(maxNumber + " " + secondMaxNumber);
+        System.out.println(numbers[n - 1] + " " + numbers[n - 2]);
     }
 }
