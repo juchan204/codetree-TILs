b991f68e252c4bf4af7f4cd0593e87891febd3fc
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-06-08 15:24:03 +0900

    Title: 가장 짧은 문자열, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240608/가장 짧은 문자열/shortest-string.java
+++ 240608/가장 짧은 문자열/shortest-string.java
@@ -24,3 +24,32 @@ public class Main {
         System.out.println(maxLength - minLength);
     }
 }
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // 문자열을 입력받습니다.
+//         String str1 = sc.next();
+//         String str2 = sc.next();
+//         String str3 = sc.next();
+
+//         // 문자열의 길이를 구합니다.
+//         int len1 = str1.length();
+//         int len2 = str2.length();
+//         int len3 = str3.length();
+
+//         // 세 문자열 중 가장 긴 길이와 가장 짧은 길이를 구합니다.
+//         int mx = len1;
+//         if(mx < len2) mx = len2;
+//         if(mx < len3) mx = len3;
+//         int mn = len1;
+//         if(mn > len2) mn = len2;
+//         if(mn > len3) mn = len3;
+
+//         // 문제에서 구하고자 하는 값을 출력합니다.
+//         System.out.print(mx - mn);
+//     }
+// }
