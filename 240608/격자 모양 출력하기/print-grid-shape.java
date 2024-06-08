9006044a020fb6b3a2e14ec3bb1ac246af26a393
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-06-08 14:51:37 +0900

    Title: 격자 모양 출력하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240608/격자 모양 출력하기/print-grid-shape.java
+++ 240608/격자 모양 출력하기/print-grid-shape.java
@@ -2,26 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-
+        // 2차원 배열을 구현합니다.
+        int[][] arr = new int[11][11];
+        
+        // n, m을 입력받습니다.
         int n = sc.nextInt();
-        int[][] placed = new int[n][n];
-
         int m = sc.nextInt();
-
-        for (int i = 0; i < m; i++) {
-            int r = sc.nextInt() - 1 ; 
-            int c = sc.nextInt() - 1;
-            placed[r][c] = (r+1)*(c+1);
+        
+        // m회에 걸쳐 동전의 위치를 입력받고 올바른 위치에 1을 표기합니다.
+        for(int i = 0; i < m; i++) {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            arr[r][c] = r * c;
         }
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                System.out.print(placed[i][j] + " ");
+                
+        // 채워진 배열을 출력합니다.
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
-
     }
 }
