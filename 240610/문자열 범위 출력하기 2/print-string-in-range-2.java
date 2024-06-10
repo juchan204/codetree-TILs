20a16ccdfbdaa6c85f420166c03428f2f6d8ba78
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-06-10 16:13:37 +0900

    Title: 문자열 범위 출력하기 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240610/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 240610/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -4,15 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 문자열 str을 입력받음
         String str = sc.next();
-
+        // 정수 n을 입력받음
         int n = sc.nextInt();
 
-        int len = str.length();
+        // str의 길이가 n보다 작으면 런타임 에러가 발생할 수 있으므로 조건을 추가
+        if (n > str.length()) {
+            System.out.println("Error: n is larger than the length of the string.");
+            return;
+        }
 
-        for(int i = len - 1; i >= len - n ; i--){
+        // 문자열의 끝에서부터 n개의 문자를 출력
+        for(int i = str.length() - 1; i >= str.length() - n ; i--) {
             System.out.print(str.charAt(i));
         }
-            
     }
 }
