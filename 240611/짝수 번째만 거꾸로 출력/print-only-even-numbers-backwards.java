49674aaa9935469610eb4e42aa849593b70f899f
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-06-11 22:09:42 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240611/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
+++ 240611/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -4,33 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 첫 번째 줄에 문자열 입력받음
-        String str = sc.nextLine();
+        // 문자열을 구현합니다.
+        String str = sc.next();
 
-        // 문자열의 길이
-        int length = str.length();
+        // 가장 먼저 출력해야 할 문자의 인덱스를 구합니다.
+        int idx = str.length() - 1;
+        if(idx % 2 == 0) idx--;
 
-        // 짝수 번째 문자의 개수를 계산
-        int evenCharCount = length / 2;
-
-        // 짝수 번째 문자를 저장할 배열 생성
-        char[] evenChars = new char[evenCharCount];
-
-        // 짝수 번째 문자 추출
-        int index = 0;
-        for (int i = 1; i < length; i += 2) {
-            evenChars[index++] = str.charAt(i);
+        // 문자열을 순회하여 조건대로 문자를 출력합니다.
+        for(int i = idx; i >= 0; i -= 2) {
+            System.out.print(str.charAt(i));
         }
-
-        // 결과를 저장할 StringBuilder
-        StringBuilder result = new StringBuilder();
-
-        // 역순으로 배열을 순회하며 결과에 추가
-        for (int i = evenCharCount - 1; i >= 0; i--) {
-            result.append(evenChars[i]);
-        }
-
-        // 결과 출력
-        System.out.println(result.toString());
     }
 }
