8293610ee125b42f7179d6de491f1c7342c41a6c
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-06-14 21:20:02 +0900

    Title: 문자 교체하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240614/문자 교체하기/changing-char.java
+++ 240614/문자 교체하기/changing-char.java
@@ -8,11 +8,8 @@ public class Main {
         String str1 = sc.next();
         String str2 = sc.next();
 
-        // 문자열의 길이를 구합니다.
-        int len = str2.length();
-
         // 문자열의 앞에서 두 번째 원소와 뒤에서 두 번째 원소를 'a'로 대체합니다.
-        str2 = str1.substring(0, 2)+ str2.substring(2, len);
+        str2 = str1.substring(0, 2)+ str2.substring(2);
             
         // 대체된 이후의 문자열을 출력합니다
         System.out.println(str2);
