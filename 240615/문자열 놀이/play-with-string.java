4e57760dfda3919f4af62fb1776d100f81e5650c
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-06-15 17:21:24 +0900

    Title: 문자열 놀이, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240615/문자열 놀이/play-with-string.java
+++ 240615/문자열 놀이/play-with-string.java
@@ -4,41 +4,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 문자열과 질의 수 입력
-        String s = sc.next();
+        // 문자열을 입력받습니다.
+        String str = sc.next();
         int q = sc.nextInt();
-        sc.nextLine();  // 개행 문자 소비
 
-        // 문자열을 변형 가능한 StringBuilder로 변환
-        StringBuilder str = new StringBuilder(s);
+        // 문자열의 길이를 구합니다.
+	    int len = str.length();
 
-        // 질의 처리
-        for (int i = 0; i < q; i++) {
-            String query = sc.nextLine();
-            String[] parts = query.split(" ");
+        // q개의 질의를 수행합니다.
+        for(int i = 0; i < q; i++) {
+            // 몇번째 질의인지 확인합니다.
+            int qType = sc.nextInt();
+            
+            if(qType == 1) {
+                // a, b를 입력받은 후, a번째 문자와 b번째 문자를 교환하여 출력합니다.
+                int a = sc.nextInt();
+                int b = sc.nextInt();
 
-            int type = Integer.parseInt(parts[0]);
-            if (type == 1) {
-                // 1 a b: a번째 문자와 b번째 문자를 교환
-                int a = Integer.parseInt(parts[1]) - 1;
-                int b = Integer.parseInt(parts[2]) - 1;
-                char temp = str.charAt(a);
-                str.setCharAt(a, str.charAt(b));
-                str.setCharAt(b, temp);
-            } else if (type == 2) {
-                // 2 a b: 문자 a를 전부 문자 b로 변경
-                char a = parts[1].charAt(0);
-                char b = parts[2].charAt(0);
-                for (int j = 0; j < str.length(); j++) {
-                    if (str.charAt(j) == a) {
-                        str.setCharAt(j, b);
-                    }
-                }
+                char tmp = str.charAt(a - 1);
+                
+                // a번째 문자의 자리에 b번째 문자를 넣습니다.
+                str = str.substring(0, a - 1) + str.charAt(b - 1) + str.substring(a);
+                
+                // b번째 문자의 자리에 a번째 문자를 넣습니다.
+                str = str.substring(0, b - 1) + tmp + str.substring(b);
+
+                System.out.println(str);
             }
-            // 변형된 문자열 출력
-            System.out.println(str.toString());
+            else {
+                // a, b를 입력받은 후, 문자 a를 전부 b로 변경한 뒤 출력합니다.
+                char a = sc.next().charAt(0);
+                char b = sc.next().charAt(0);
+                
+                // 문자 a를 전부 b로 변경합니다.
+                for(int j = 0; j < len; j++)
+                    if(str.charAt(j) == a)
+                        str = str.substring(0, j) + b + str.substring(j + 1);
+                
+                System.out.println(str);
+            }
         }
-
-        sc.close();
     }
 }
