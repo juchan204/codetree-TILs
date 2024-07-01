038e297820b412abc5d673953713b06b1da44796
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-07-01 22:09:08 +0900

    Title: 문자열 밀기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240701/문자열 밀기/push-char.java
+++ 240701/문자열 밀기/push-char.java
@@ -11,3 +11,25 @@ public class Main {
         System.out.println(shiftedStr);
     }
 }
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // 문자열을 정의합니다.
+//         String str;
+        
+//         // 문자열을 입력받습니다.
+//         str = sc.next();
+        
+//         // 문자열의 길이를 구합니다.
+//         int len = str.length();
+        
+//         // 문자열을 왼쪽으로 한 칸 쉬프트합니다.
+//         str = str.substring(1) + str.substring(0, 1);
+        
+//         // 왼쪽으로 한 칸 쉬프트된 문자열을 출력합니다.
+//         System.out.println(str);
+//     }
+// }
