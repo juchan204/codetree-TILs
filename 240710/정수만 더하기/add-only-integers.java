c96663754bf8b6ab0329abae2520e0edb4715d78
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-07-10 16:54:04 +0900

    Title: 정수만 더하기, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

--- 240710/정수만 더하기/add-only-integers.java
+++ 240710/정수만 더하기/add-only-integers.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+
+        // 문자열을 정의합니다.
+        String str;
+        int ans = 0;
         
-        int sum = 0;
-        
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            if (Character.isDigit(ch)) {
-                int digit = Character.getNumericValue(ch); // char를 int로 변환하여 숫자 값을 가져옴
-                sum += digit;
-            }
+        // 문자열을 입력받습니다.
+        str = sc.next();
+
+        // 문자열을 순회하여 숫자들만 골라 더해줍니다.
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) >= '0' && str.charAt(i) <= '9')
+                ans += (str.charAt(i) - '0');
         }
-        
-        System.out.println(sum);
+
+        System.out.print(ans);
     }
 }
