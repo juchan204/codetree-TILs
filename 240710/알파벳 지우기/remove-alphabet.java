7cb7401a53b5227f2385ecf52755d59f3642e18f
이주찬 <127003137+juchan204@users.noreply.github.com>
2024-07-10 16:57:28 +0900

    Title: 알파벳 지우기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240710/알파벳 지우기/remove-alphabet.java
+++ 240710/알파벳 지우기/remove-alphabet.java
@@ -2,33 +2,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        // 문자열을 정의합니다.
+        String a;
+        String b;
+        String str1 = "";
+        String str2 = "";
         
-        // 첫 번째 문자열 입력
-        String str1 = scanner.nextLine();
-        // 두 번째 문자열 입력
-        String str2 = scanner.nextLine();
+        // 문자열을 입력받습니다.
+        a = sc.next();
+        b = sc.next();
+
+        // 문자열의 길이를 구합니다.
+	    int lenA = a.length();
+	    int lenB = b.length();
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        // a의 정수로 변환 가능한 부분을 다른 문자열로 옮깁니다.
+        for(int i = 0; i < lenA; i++)
+            if(a.charAt(i) <= '9' && a.charAt(i) >= '0')
+                str1 += a.charAt(i);
+
+        // b의 정수로 변환 가능한 부분을 다른 문자열로 옮깁니다.
+        for(int i = 0; i < lenB; i++)
+            if(b.charAt(i) <= '9' && b.charAt(i) >= '0')
+                str2 += b.charAt(i);
         
-        // 각 문자열에서 숫자 부분 추출하여 정수로 변환
-        int num1 = extractNumber(str1);
-        int num2 = extractNumber(str2);
+        // 합쳐진 문자열을 숫자로 바꿉니다.
+        int str1Int = Integer.parseInt(str1);
+        int str2Int = Integer.parseInt(str2);
         
-        // 두 수의 합 계산
-        int sum = num1 + num2;
-        
-        // 결과 출력
-        System.out.println(sum);
-    }
-    
-    // 문자열에서 숫자 부분 추출하여 정수로 변환하는 메소드
-    private static int extractNumber(String str) {
-        StringBuilder sb = new StringBuilder();
-        for (char ch : str.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                sb.append(ch);
-            }
-        }
-        String numStr = sb.toString();
-        return Integer.parseInt(numStr);
+        // 두 숫자의 합을 출력합니다.
+        System.out.print(str1Int + str2Int);
     }
 }
